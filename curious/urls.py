40d93aa4a1464e885ae8bed33634d275be1202a0
Benjie Chen <benjie@ginkgobioworks.com>
2014-05-08 08:51:59 -0400

    Can return main app page via Django, not just static

--- curious/urls.py
+++ curious/urls.py
@@ -1,9 +1,23 @@
 from django.conf.urls import *
+from django.views.generic.base import TemplateView
+from django.http import HttpResponseRedirect
 from .api import ObjectView, ModelView, ModelListView, QueryView
+
+def redirect_to_static(request):
+  path = request.get_full_path()
+  return HttpResponseRedirect('/static%s' % path)
 
 urlpatterns = patterns('',
   url(r'^objects/(?P<model_name>[\w\-]+)/(?P<id>[\w\-]+)/$', ObjectView.as_view()),
   url(r'^models/(?P<model_name>[\w\-]+)/$', ModelView.as_view()),
   url(r'^models/$', ModelListView.as_view()),
   url(r'^q/$', QueryView.as_view()),
+
+  # sometimes you need to get to the curious query page via Django, e.g. to
+  # work with authentication. here we serve the curious.html via Django
+  # template engine, and add redirects for relative URLs so they match the
+  # curious/static directory tree.
+  url(r'^$', TemplateView.as_view(template_name='curious/curious.html'), name='curious'),
+  url(r'^dist', redirect_to_static),
+  url(r'^lib', redirect_to_static),
 )
