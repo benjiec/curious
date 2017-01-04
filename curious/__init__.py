5fb7ea6e69b90959d687cd2a0512076635fd4ac8
Chris Mitchell <chris@ginkgobioworks.com>
2017-01-04 14:46:39 -0500

    Fix ambiguous model name error

--- curious/__init__.py
+++ curious/__init__.py
@@ -84,7 +84,7 @@ class ModelRegistry(object):
       if name in self.__short_names:
         if len(self.__short_names[name]) != 1:
           raise Exception('Ambiguous model name "%s": can match to %s' %
-                          (name, ', '.join(self.__short_names[name])))
+                          (name, ', '.join(self.__short_names[name].short_name)))
         else:
           model_name = self.__short_names[name][0].model_name
       else:
