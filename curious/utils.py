c3df9c71363132a39df0fd1a0715524c69bb4cc1
Chris Mitchell <chris@ginkgobioworks.com>
2017-05-19 11:12:46 -0400

    Only print debug messages if DEBUG env var is set

--- curious/utils.py
+++ curious/utils.py
@@ -1,10 +1,13 @@
 import time
+import os
 
 # for development/debugging
 def report_time(f):
+  DEBUG = os.environ.get('DEBUG')
   def wrap(*args, **kwargs):
     t = time.time()
     r = f(*args, **kwargs)
-    print '%s.%s: %.4f' % (f.__module__, f.func_name, time.time()-t)
+    if DEBUG:
+      print '%s.%s: %.4f' % (f.__module__, f.func_name, time.time()-t)
     return r
   return wrap
