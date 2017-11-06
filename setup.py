a09fe1606b59bc723864d958f4c89304d68a36eb
Misha Wolfson <misha@ginkgobioworks.com>
2017-11-06 07:16:41 +0000

    Bump version: 1.0.0 → 1.0.1

--- setup.py
+++ setup.py
@@ -93,7 +93,7 @@ class WebassetsBuildCommand(Command):
 
 setup(
   name='curious',
-  version='1.0.0',
+  version='1.0.1',
 
   author='Benjie Chen, Ginkgo Bioworks',
   author_email='benjie@ginkgobioworks.com, devs@ginkgobioworks.com',
